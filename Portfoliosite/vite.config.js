8b06ce74bcd46894ae652ef8a472425dfc90e0a4
Babawale Owootori <babawaleowootori@gmail.com>
2026-02-12 02:00:29 +0100

    Portfolio ready for deployment

--- Portfoliosite/vite.config.js
+++ Portfoliosite/vite.config.js
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  root: ".", // root is the project folder
+  build: {
+    outDir: "dist", // default is 'dist'
+  },
 });
