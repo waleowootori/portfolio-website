77ba676e2e84f4becedc323440147fb2f27a7404
Babawale Owootori <babawaleowootori@gmail.com>
2026-04-27 22:38:20 +0100

    Fix Netlify deployment configuration

--- Portfoliosite/vite.config.js
+++ Portfoliosite/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  root: ".", // root is the project folder
-  build: {
-    outDir: "dist", // default is 'dist'
-  },
+  plugins: [react()],
 });
